d3f3a657b020aef9398711cecb5f11e6464e474c
Adam Fischbein <adamfischbein2005@gmail.com>
2025-11-21 23:57:38 +0100

    Initial commit

---
+++ C#_OOP_RPG_Project/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace C__OOP_RPG_Project
+{
+    abstract class GameCharacter
+    {
+        string name;
+        int health;
+        int maxHealth;
+
+        public string Name { get; }
+        public int Health { get; private set; }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}

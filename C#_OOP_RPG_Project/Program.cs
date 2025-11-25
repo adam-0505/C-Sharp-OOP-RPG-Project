c8719e03e8410d5d29b5f641ef926d57b234b921
Adam <adamfischbein2005@gmail.com>
2025-11-25 14:18:19 +0100

    added enum

--- C#_OOP_RPG_Project/Program.cs
+++ C#_OOP_RPG_Project/Program.cs
@@ -1,10 +1,18 @@
 ﻿namespace C__OOP_RPG_Project
 {
+    public enum CharacterClass
+        {
+            Warrior,
+            Mage,
+            Archer,
+            Healer
+        }        
     abstract class GameCharacter
     {
         public string Name { get; }
         public int Health { get; private set; }
         public int MaxHealth { get; }
+        public CharacterClass Role { get; set; }        
 
         public GameCharacter(string name, int maxHealth = 100)
         {
